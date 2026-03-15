12624f27d2fbdbed9eb64204e730c919bd6f8eba
Priyansh Kamlesh <priyansh1kamlesh0@gmail.com>
2026-03-15 08:20:20 +0530

    Use environment variable for server port
    Update app.run to use environment variable for port

--- app.py
+++ app.py
@@ -34,4 +34,5 @@ app.register_blueprint(otp_bp)
 app.register_blueprint(virtual_coach_bp)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    port=int(os.environ.get("PORT",5001)
+    app.run(host="0.0.0.0", port=port)
